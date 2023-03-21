d80d98855d52039f17cadb06cdd33ee8a20e12c8
Queren Hapuque <queren.fernandes@yahoo.com>
2023-03-21 20:27:32 -0300

    promisse all

--- scrip.js
+++ scrip.js
@@ -1,4 +1,22 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {console.log(r)})
-.catch(erro => console.log(erro));
+async function buscaEndereco(cep){
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/{cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if(consultaCEPConvertida.erro){
+            throw Error('CEP não existente');
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch(erro){
+        console.log(erro)
+    }
+    
+}
+
+/* CÓDIGO PRO CASO DE SER NECESSÁRIO FAZER VÁRIAS REQUISIÇÕES AO MESMO TEMPO.
+let ceps = ['01001000','01001002'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(cep));
+
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+ */
+
